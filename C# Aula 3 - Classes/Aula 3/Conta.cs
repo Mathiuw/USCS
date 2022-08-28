45ffc876b2280c9086ff257dd71596e1ff860533
Mateus Sartori <mateus.augusto2003@hotmail.com>
2022-08-27 21:17:51 -0300

    Aula 3 - Classes

---
+++ C# Aula 3 - Classes/Aula 3/Conta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_3
+{
+    class Conta
+    {
+        public int numero;
+        public Cliente titular;
+        public double saldo;
+
+        public double Mostrasaldo()
+        {
+            return this.saldo;
+        }
+
+        public int MostraNumero()
+        {
+            return this.numero;
+        }
+
+        public string MostraTitular()
+        {
+            return titular.nome + " " + titular.cpf;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo >= valor)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else return false;
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool transferir(double valor, Conta conta)
+        {
+            if (this.saldo >= valor)
+            {
+                this.saldo -= valor;
+                conta.saldo += valor;
+                return true;
+            }
+            else return false;
+        }
+    }
+}
