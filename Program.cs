b5750038bb1bb3edf4f7a9cbe293a9c298b719be
Mateus Sartori <98856859+Mathiuw@users.noreply.github.com>
2022-09-09 20:42:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_5_Get_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Ana", 23, 'F');
+            Pessoa p2 = new Pessoa("Paulo", 35, 'M');
+            Pessoa p3 = new Pessoa("Lia", 12, 'F');
+
+            Console.WriteLine(p1.GetNome() + " " + p1.GetIdade() + " " + p1.GetSexo());
+            Console.WriteLine(p2.GetNome() + " " + p2.GetIdade() + " " + p2.GetSexo());
+            Console.WriteLine(p3.GetNome() + " " + p3.GetIdade() + " " + p3.GetSexo());
+            Console.ReadKey();
+        }
+    }
+}
