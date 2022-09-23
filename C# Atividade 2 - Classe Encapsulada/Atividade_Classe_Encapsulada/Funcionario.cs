8877409bf1f6a739aa406af908bbeaafa883527c
Mateus Sartori <mateus.augusto2003@hotmail.com>
2022-09-23 12:53:30 -0300

    Atividade 2 - Classe Encapsulada

---
+++ C# Atividade 2 - Classe Encapsulada/Atividade_Classe_Encapsulada/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Classe_Encapsulada
+{
+    internal class Funcionario
+    {
+        string nome;
+        public string Nome { get => nome; set => nome = value; }
+        
+        int matricula;
+        public int Matricula { get => matricula; set => matricula = value; }
+        
+        int salario;
+        public int Salario { get => salario; set => salario = value; }
+
+        public Funcionario(string nome, int matricula, int salario)
+        {
+            Nome = nome;
+            Matricula = matricula;
+            Salario = salario;
+        }
+    }
+}
---
+++ C# Atividade 2 - Classe Encapsulada/Atividade_Classe_Encapsulada/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Classe_Encapsulada
+{
+    internal class Program
+    {
+        public static float media = 0;
+
+        static void Main(string[] args)
+        {
+            Funcionario[] funcionarios = new Funcionario[4];
+
+            funcionarios[0] = new Funcionario("Carlos", 256, 1200);
+            funcionarios[1] = new Funcionario("Roberto", 486, 1800);
+            funcionarios[2] = new Funcionario("Claudinei", 158, 2000);
+            funcionarios[3] = new Funcionario("Pedro", 129, 1000);
+
+            MediaSalarial(funcionarios);
+            Console.WriteLine("\nSalarios Acima da Media : ");
+            ExibirSalarioAcimaDaMedia(funcionarios, media);
+            Console.ReadKey();
+        }
+
+        static void MediaSalarial(Funcionario[] funcionarios) 
+        {
+            foreach (Funcionario f in funcionarios) media += f.Salario;
+            media /= funcionarios.Length;
+            Console.WriteLine("Media Salarial = " + media );      
+        }
+
+        static void ExibirSalarioAcimaDaMedia(Funcionario[] funcionarios, float media) 
+        {
+            foreach (Funcionario f in funcionarios) if (f.Salario > media) Console.WriteLine(f.Nome + " " + f.Matricula);        
+        }
+    }
+}
